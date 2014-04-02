7c01bb759f11a8a4028c3a2129902c81ca7192eb
Andrey F. Mindubaev <covex.mobile@gmail.com>
2014-04-02 21:00:42 +0400

    Invalidate di container after changes in config.rb files

--- src/Apnet/AsseticImporterBundle/DependencyInjection/Compiler/CollectionResourcePass.php
+++ src/Apnet/AsseticImporterBundle/DependencyInjection/Compiler/CollectionResourcePass.php
@@ -11,6 +11,7 @@ namespace Apnet\AsseticImporterBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Config\Resource\FileResource;
 
 /**
  * Adds services tagged as assets to the resource collection
@@ -23,11 +24,31 @@ class CollectionResourcePass implements CompilerPassInterface
    */
   public function process(ContainerBuilder $container)
   {
+    $obj = $container->getDefinition("apnet.assetic.importer.compass_project1");
+    $obj->getProperties();
+
     $collection = $container->getDefinition('apnet.assetic.importer_resource');
 
     $mappers = $container->findTaggedServiceIds('apnet.assetic.asset_mapper');
-    foreach ($mappers as $id => $attributes) {
-      $collection->addMethodCall('addAssetMapper', array(new Reference($id)));
+    foreach ($mappers as $id => $tagAttributes) {
+      foreach ($tagAttributes as $attr) {
+        if (isset($attr["config"])) {
+          $config = $attr["config"];
+          $mapper = $container->getDefinition($id);
+          $arguments = $mapper->getArguments();
+
+          if (isset($arguments[$config])) {
+            $configPath = $container->getParameterBag()
+              ->resolveValue($arguments[$config]);
+
+            if (file_exists($configPath)) {
+              $configResource = new FileResource($configPath);
+              $container->addResource($configResource);
+            }
+          }
+        }
+        $collection->addMethodCall('addAssetMapper', array(new Reference($id)));
+      }
     }
   }
 
