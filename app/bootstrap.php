c002658d2e2d17ab2b3759e2a7b2f08ba96e15aa
Andrey F. Mindubaev <covex.mobile@gmail.com>
2014-05-03 23:04:47 +0400

    Updated bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,9 +8,7 @@ if (!file_exists(dirname(__DIR__) . "/vendor/autoload.php")) {
     'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
     'php composer.phar install' . PHP_EOL;
 } else {
-  $loader = include dirname(__DIR__) . "/vendor/autoload.php";
-  $loader->add("Apnet\\Dev\\", __DIR__ . "/src");
-  $loader->register();
+  /* $loader = */include dirname(__DIR__) . "/vendor/autoload.php";
 
   chdir(dirname(__DIR__));
   Debug::enable();
